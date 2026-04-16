d83b042d828299d4abee7d17d96656479bff4241
Martromeda <martinatybur@gmail.com>
2026-04-16 19:20:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-user8" #change here
-    storage_account_name = "user8sa" #change here
+    resource_group_name  = "rg-user8"
+    storage_account_name = "user8sa"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }

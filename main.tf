87739cc4a61699e82708d417bc0f1bcc53d4229e
Martromeda <martinatybur@gmail.com>
2026-04-16 19:52:28 +0200

    Update main.tf
    Added keyvault

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = ">=4.1.0"
     }
   }
 }
@@ -17,4 +17,15 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
+module "keyvault" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
+  keyvault_name = "gakvuser82026"
+  resource_group = {
+    location = "northeurope"
+    name     = "rg-user8"
+    }
+  network_acls = {
+    default_action             = "Deny"
+    bypass                     = "AzureServices"
+    }
 }
